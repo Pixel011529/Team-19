7ec2a0a36af69fc9daad3f1d29d581a466f443e9
Naveen Selvam R <naveenselvam15@gmail.com>
2020-10-09 15:17:34 +0530

    add motion detector (new)

--- BackEnd/motionDetection.py
+++ BackEnd/motionDetection.py
@@ -1,20 +1,28 @@
 import numpy as np
 import cv2
+import time
 
 class MotionDetector:
 
-    def __init__(self, minSizeForMotion):
+    def __init__(self, minSizeForMotion, framesToPersist, movementDetectedPersistence, firstFrame):
 
         self.minSizeForMotion = minSizeForMotion
         self.isMotionDetected = False
+        self.framesToPersist = framesToPersist
+        self.movementDetectedPersistence = movementDetectedPersistence
+        self.delayCounter = 0
+        self.movementPersistenceCounter = 0
+        self.firstFrame = firstFrame
 
     def motionAnalyzer(self, currFrame, prevFrame):
 
+        gray = cv2.GaussianBlur(currFrame, (21, 21), 0)
         frameDiff = cv2.absdiff(currFrame, prevFrame)
         thresh = cv2.threshold(frameDiff, 25, 255, cv2.THRESH_BINARY)[1]
         thresh = cv2.dilate(thresh, None, iterations = 2)
         contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+        time.sleep(0.1)
         for contour in contours:
             if cv2.contourArea(contour) > self.minSizeForMotion:
                 self.isMotionDetected = True

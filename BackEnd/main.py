6ef806f56c90965cad79fe9e27fa999493629ec7
Naveen Selvam R <naveenselvam15@gmail.com>
2020-10-09 20:07:18 +0530

    human detection

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -4,6 +4,9 @@ import time
 import imutils
 from frame import *
 from motionDetection import *
+from humanDetector import *
+from timer import *
+from rooms import *
 
 
 cap = cv2.VideoCapture(0) 
@@ -11,35 +14,52 @@ cap = cv2.VideoCapture(0)
 
 ret, frame = cap.read()
 frame = imutils.resize(frame, width = 750)
-frame = cv2.GaussianBlur(frame, (21, 21), 0)
+
 prevFrame = Frame(frame)
 
+room = Rooms(2, 3)
 motionDetector = MotionDetector(2500, 10, 100, prevFrame)
+humanDetector = HumanDetector(2500)
+timer = Timer(2)
 
 while (cap.isOpened()):
 
     cv2.waitKey(1)
     ret, frame = cap.read()
     frame = imutils.resize(frame, width = 750)
-    frame = cv2.GaussianBlur(frame, (21, 21), 0)
 
     if ret == False:
         continue
 
     currFrame = Frame(frame)
 
-
     contours = motionDetector.analyze(currFrame)
 
-    displayFrame = currFrame.frameBGR.copy()
-
-    
-    cv2.imshow("frame", displayFrame)
-
-    
     ch = cv2.waitKey(1)
     if ch & 0xFF == ord('q'):
         break
+
+    if ch == ord('r'):
+        room.restart()
+
+    if timer.frameToBeSkipped(time.time()):
+        motionDetector.firstFrame = currFrame
+        continue
+
+    displayFrame = currFrame.frameBGR.copy()
+
+    if motionDetector.movementPersistentCounter >= 98:
+        displayFrame = humanDetector.analyzeYOLO(displayFrame)
+        timer.update(time.time())
+
+    room.update(humanDetector)
+    
+    
+
+    roomInfo = room.displayImage()
+
+    cv2.imshow("YOLO object detection and Room status", np.hstack((displayFrame, roomInfo)))
+
 
 cv2.destroyAllWindows()
 cap.release()
---
+++ BackEnd/rooms.py
@@ -0,0 +1,37 @@
+import numpy as np
+import cv2
+
+class Rooms:
+
+    def __init__(self, floorNo, roomNo):
+
+        self.floorNo = floorNo
+        self.roomNo = roomNo
+        self.bluePrint = None   
+        self.noOfIntruders = 0
+        self.noOfWeapons = 0
+        self.isSecure = True
+
+    def displayImage(self):
+
+        image = np.zeros((562, 750, 3), np.uint8)
+        cv2.putText(image, "Floor No: " + str(self.floorNo), (10,35), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255,255,255), 3, cv2.LINE_AA)
+        cv2.putText(image, "Room No: " + str(self.roomNo), (10,95), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255,255,255), 3, cv2.LINE_AA)
+        cv2.putText(image, "No of Intruders: " + str(self.noOfIntruders), (10,155), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255,255,255), 3, cv2.LINE_AA)
+        cv2.putText(image, "No of Weapons: " + str(self.noOfWeapons), (10,215), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255,255,255), 3, cv2.LINE_AA)
+        cv2.putText(image, "Is Secure: " + str(self.isSecure), (10,275), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255,255,255), 3, cv2.LINE_AA)
+
+        return image 
+    
+    def update(self, humanDetector):
+
+        self.noOfIntruders = max(self.noOfIntruders, humanDetector.noOfHumans)
+
+        if self.noOfIntruders > 0:
+            self.isSecure = False
+
+    def restart(self):
+
+        self.noOfIntruders = 0
+        self.noOfWeapons = 0
+        self.isSecure = True

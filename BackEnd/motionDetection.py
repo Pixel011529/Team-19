799c4c40150b6a1d0b792944c467761e350541e2
Naveen Selvam R <naveenselvam15@gmail.com>
2020-10-09 18:23:29 +0530

    refactor motion detection and yolo installation

--- BackEnd/motionDetection.py
+++ BackEnd/motionDetection.py
@@ -1,35 +1,59 @@
 import numpy as np
 import cv2
 import time
+import imutils
 
 class MotionDetector:
 
-    def __init__(self, minSizeForMotion, framesToPersist, movementDetectedPersistence, firstFrame):
+    def __init__(self, minSizeForMotion, framesToPersist, movementDetectedPersistence, frame):
 
         self.minSizeForMotion = minSizeForMotion
-        self.isMotionDetected = False
         self.framesToPersist = framesToPersist
         self.movementDetectedPersistence = movementDetectedPersistence
         self.delayCounter = 0
-        self.movementPersistenceCounter = 0
-        self.firstFrame = firstFrame
+        self.movementPersistentCounter = 0
+        self.isTransientMotionDetected = False
+        self.isMovementPersistent = False
+        self.firstFrame = frame
 
-    def motionAnalyzer(self, currFrame, prevFrame):
+    def analyze(self, currFrame):
 
-        gray = cv2.GaussianBlur(currFrame, (21, 21), 0)
-        frameDiff = cv2.absdiff(currFrame, prevFrame)
+        
+        frameDiff = cv2.absdiff(currFrame.frameGrey, self.firstFrame.frameGrey)
         thresh = cv2.threshold(frameDiff, 25, 255, cv2.THRESH_BINARY)[1]
         thresh = cv2.dilate(thresh, None, iterations = 2)
-        contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        time.sleep(0.1)
-        for contour in contours:
-            if cv2.contourArea(contour) > self.minSizeForMotion:
-                self.isMotionDetected = True
-                return thresh, contours
         
-        self.isMotionDetected = False
-        return thresh, contours     
+        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        self.delayCounter += 1
+        if self.delayCounter > self.framesToPersist:
+            self.delayCounter = 0
+            self.firstFrame = currFrame
+
+        self.isTransientMotionDetected = False
+
+        for contour in contours:
+            (x, y, w, h) = cv2.boundingRect(contour)
+            if cv2.contourArea(contour) > self.minSizeForMotion:
+                self.isTransientMotionDetected = True
+        
+        if self.isTransientMotionDetected:
+            self.isMovementPersistent = True
+            self.movementPersistentCounter = self.movementDetectedPersistence 
+
+        if self.movementPersistentCounter > 0:
+            text = "Movement Detected " + str(self.movementPersistentCounter)
+            self.movementPersistentCounter -= 1
+        else:
+            text = "No Movement Detected"
+
+        thresh = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
+        cv2.imshow("thresh", frameDiff)
+
+        return contours
+        
+        
+                
     
     def drawContours(self, frame, contours):
         

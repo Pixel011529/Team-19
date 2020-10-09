8cd797c801dcb2da5a910103205b2401fefcd91f
ImElan <elansakthivel2000@gmail.com>
2020-10-09 22:58:15 +0530

    render MyHousePage page with images from database

--- FrontEnd/pixels/src/Components/MyHousePageComponents/MyHouseContainer.js
+++ FrontEnd/pixels/src/Components/MyHousePageComponents/MyHouseContainer.js
@@ -6,73 +6,51 @@ import axios from 'axios';
 
 import secure from '../../assets/secure.svg';
 import danger from '../../assets/danger.svg';
+import blueprint from '../../assets/floorPlan.png';
 
 class MyHouseContainer extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			images: null,
-			isSecure: 'True',
+			roomOneImages: null,
+			roomOneisSecure: 'True',
+			roomOnenumIntruders: '',
+			roomOnefloor: '',
+			roomOneroom: '',
+			roomOnenumWeapons: '',
 			address: 'No 2, Manhattan,Central Perk, NY',
-			numIntruders: '',
-			floor: '',
-			room: '',
-			numWeapons: '',
 			monitoring: true,
+			roomTwoImages: null,
+			roomTwoisSecure: 'True',
+			roomTwonumIntruders: '',
+			roomTwofloor: '',
+			roomTworoom: '',
+			roomTwonumWeapons: '',
 		};
 		this.parseImage = this.parseImage.bind(this);
         this.handleMonitoring = this.handleMonitoring.bind(this);
         this.handleReset = this.handleReset.bind(this);
-	}
-	componentDidMount() {
-		const idToken = window.localStorage.getItem('idToken');
-		axios.get(`https://hackathon-94220.firebaseio.com/Images.json?auth=${idToken}`)
-			.then((response) => {
-                if(response.data != null) {
-                    this.setState({
-                        images: this.parseImage(response.data.slice(1)),
-                    });
-                }
-			})
-			.catch((error) => {
-				console.log(error);
-			});
+        this.getRoomOneDetails = this.getRoomOneDetails.bind(this);
+        this.getRoomTwoDetails = this.getRoomTwoDetails.bind(this);
+    }
 
-		axios.get(`https://hackathon-94220.firebaseio.com/Details.json?auth=${idToken}`)
-			.then((response) => {
-                if(response.data != null) {
-                    const data = response.data;
-                    this.setState({
-                        isSecure: data.IsSecure,
-                        numIntruders: data.NoOfIntruders,
-                        floor: data.FloorNo,
-                        room: data.RoomNo,
-                        numWeapons: data.NoOfWeapons,
-                    });
-                }
-			})
-			.catch((error) => {
-				console.log(error);
-            });
-            
-        axios.get(`https://hackathon-94220.firebaseio.com/Monitoring.json`)
-            .then(response => {
-                this.setState({
-                    monitoring:response.data.toMonitor
-                })
-            })
-            .catch(error => {
-                console.log(error);
-            })
+    componentDidMount() {
+		this.getRoomOneDetails();
+		this.getRoomTwoDetails();
     }
     
 	componentDidUpdate() {
-		const idToken = window.localStorage.getItem('idToken');
-		axios.get(`https://hackathon-94220.firebaseio.com/Images.json?auth=${idToken}`)
+        this.getRoomOneDetails();
+        this.getRoomTwoDetails();
+	}
+    
+    getRoomOneDetails() {
+        const idToken = window.localStorage.getItem('idToken');
+		axios.get(`https://hackathon-pixel.firebaseio.com/RoomOne.json?auth=${idToken}`)
 			.then((response) => {
                 if(response.data != null) {
                     this.setState({
-                        images: this.parseImage(response.data.slice(1)),
+                        roomOneImages: this.parseImage(response.data.slice(1)),
                     });
                 }
 			})
@@ -80,23 +58,55 @@ class MyHouseContainer extends Component {
 				console.log(error);
 			});
 
-		axios.get(`https://hackathon-94220.firebaseio.com/Details.json?auth=${idToken}`)
+		axios.get(`https://hackathon-pixel.firebaseio.com/DetailsOne.json?auth=${idToken}`)
 			.then((response) => {
                 if(response.data != null) {
                     const data = response.data;
                     this.setState({
-                        isSecure: data.IsSecure,
-                        numIntruders: data.NoOfIntruders,
-                        floor: data.FloorNo,
-                        room: data.RoomNo,
-                        numWeapons: data.NoOfWeapons,
+                        roomOneisSecure: data.IsSecure,
+                        roomOnenumIntruders: data.NoOfIntruders,
+                        roomOnefloor: data.FloorNo,
+                        roomOneroom: data.RoomNo,
+                        roomOnenumWeapons: data.NoOfWeapons,
                     });
                 }
 			})
 			.catch((error) => {
 				console.log(error);
             });
-	}
+    }
+
+    getRoomTwoDetails() {
+        const idToken = window.localStorage.getItem('idToken');
+		axios.get(`https://hackathon-pixel.firebaseio.com/RoomTwo.json?auth=${idToken}`)
+			.then((response) => {
+                if(response.data != null) {
+                    this.setState({
+                        roomTwoImages: this.parseImage(response.data.slice(1)),
+                    });
+                }
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+
+		axios.get(`https://hackathon-pixel.firebaseio.com/DetailsTwo.json?auth=${idToken}`)
+			.then((response) => {
+                if(response.data != null) {
+                    const data = response.data;
+                    this.setState({
+                        roomTwoisSecure: data.IsSecure,
+                        roomTwonumIntruders: data.NoOfIntruders,
+                        roomTwofloor: data.FloorNo,
+                        roomTworoom: data.RoomNo,
+                        roomTwonumWeapons: data.NoOfWeapons,
+                    });
+                }
+			})
+			.catch((error) => {
+				console.log(error);
+            });
+    }
 
 	parseImage(images) {
 		let parsedImages = [];
@@ -112,14 +122,14 @@ class MyHouseContainer extends Component {
     
     handleReset() {
         const idToken = window.localStorage.getItem('idToken');
-        axios.delete(`https://hackathon-94220.firebaseio.com/Details.json?auth=${idToken}`)
+        axios.delete(`https://hackathon-pixel.firebaseio.com/Details.json?auth=${idToken}`)
             .then(response => {
                 console.log(response);
             })
             .catch(error => {
                 console.log(error);
             })
-        axios.delete(`https://hackathon-94220.firebaseio.com/Images.json?auth=${idToken}`)
+        axios.delete(`https://hackathon-pixel.firebaseio.com/RoomOne.json?auth=${idToken}`)
             .then(response => {
                 console.log(response);
             })
@@ -130,27 +140,34 @@ class MyHouseContainer extends Component {
 
 	render() {
 		const {
-			images,
-			numIntruders,
-			floor,
-			room,
-			numWeapons,
-			isSecure,
+			roomOneImages,
+			roomOnenumIntruders,
+			roomOnefloor,
+			roomOneroom,
+			roomOnenumWeapons,
+			roomOneisSecure,
+			roomTwoImages,
+			roomTwonumIntruders,
+			roomTwofloor,
+			roomTworoom,
+			roomTwonumWeapons,
+			roomTwoisSecure,
 			address,
 			monitoring,
 		} = this.state;
-		const toRender = isSecure === 'False' && monitoring;
+		const toRenderOne = roomOneisSecure === 'False' && monitoring;
+		const toRenderTwo = roomTwoisSecure === 'False' && monitoring;
 		return (
 			<div className='myHouse'>
 				<div className='myHouse__status'>
 					<h1 className='heading--1 heading--1-dark'>
-						{!toRender
+						{!toRenderOne && !toRenderTwo
 							? 'Your House Is Safe'
 							: 'Your House Is Under Attack'}
 					</h1>
 					<img
 						className='myHouse__statusImage'
-						src={!toRender ? secure : danger}
+						src={!toRenderOne && !toRenderTwo ? secure : danger}
 						alt='House status'
 					/>
 				</div>
@@ -168,26 +185,27 @@ class MyHouseContainer extends Component {
 						Reset
 					</button>
 				</div>
-				{toRender && (
+                <h2 className='heading--2 heading--2-dark'>Room No : {roomOneroom}</h2>
+				{toRenderTwo && (
 					<div className='myHouse__details'>
 						<MyHouseDetails
-							detail={`${numIntruders} intruders`}
+							detail={`${roomOnenumIntruders} intruders`}
 							icon='robbery'
 						/>
-						<MyHouseDetails detail={`Floor : ${floor}`} icon='step' />
-						<MyHouseDetails detail={`Room : ${room}`} icon='exit' />
+						<MyHouseDetails detail={`Floor : ${roomOnefloor}`} icon='step' />
+						<MyHouseDetails detail={`Room : ${roomOneroom}`} icon='exit' />
 						<MyHouseDetails detail={address} icon='google-maps' />
 						<MyHouseDetails
-							detail={`${numWeapons} Weapons`}
+							detail={`${roomOnenumWeapons} Weapons`}
 							icon='assault-rifle
 '
 						/>
 					</div>
 				)}
-				{toRender && (
+				{toRenderTwo && (
 					<div className='myHouse__images'>
-						{images &&
-							images.map((image, index) => (
+						{roomTwoImages &&
+							roomTwoImages.map((image, index) => (
 								<img
 									key={index}
 									className='myHouse__images--img'
@@ -197,10 +215,45 @@ class MyHouseContainer extends Component {
 							))}
 					</div>
 				)}
-				{toRender && (
+                <hr className='contact__underline'></hr>
+                <h2 className='heading--2 heading--2-dark'>Room No : {roomTworoom}</h2>
+				{toRenderTwo && (
+					<div className='myHouse__details'>
+						<MyHouseDetails
+							detail={`${roomTwonumIntruders} intruders`}
+							icon='robbery'
+						/>
+						<MyHouseDetails detail={`Floor : ${roomTwofloor}`} icon='step' />
+						<MyHouseDetails detail={`Room : ${roomTworoom}`} icon='exit' />
+						<MyHouseDetails detail={address} icon='google-maps' />
+						<MyHouseDetails
+							detail={`${roomTwonumWeapons} Weapons`}
+							icon='assault-rifle
+'
+						/>
+					</div>
+				)}
+				{toRenderOne && (
+					<div className='myHouse__images'>
+						{roomOneImages &&
+							roomOneImages.map((image, index) => (
+								<img
+									key={index}
+									className='myHouse__images--img'
+									alt='robbery'
+									src={`data:image/jpg;base64,${image}`}
+								/>
+							))}
+					</div>
+				)}
+				{toRenderOne && toRenderTwo && (
 					<div className='myHouse__blueprint'>
 						<h2 className='heading--2 heading--2-dark'>Blueprint</h2>
-						<h3>BluePrint Image</h3>
+						<img
+                            className='myHouse__blueprintImage'
+                            alt='robbery'
+                            src={blueprint}
+                        />
 					</div>
 				)}
 			</div>
